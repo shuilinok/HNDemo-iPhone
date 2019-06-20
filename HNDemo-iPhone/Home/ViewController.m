eda6abb9b36e556ff6070394a1aac7f63b00bc1c
lin.shui <lin.shui@yinyuetai.com>
2019-06-20 22:46:59 +0800

    save

--- HNDemo-iPhone/Home/ViewController.m
+++ HNDemo-iPhone/Home/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 #import "HNKeyCourseListViewController.h"
-#import "Person.h"
+#import <objc/runtime.h>
 
 @interface ViewController ()
 
@@ -17,6 +17,33 @@
 @end
 
 @implementation ViewController
+/*
++ (void)load {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        Class class = [self class];
+        // When swizzling a class method, use the following:
+        // Class class = object_getClass((id)self);
+        SEL originalSelector = @selector(viewWillAppear:);
+        SEL swizzledSelector = @selector(xxx_viewWillAppear:);
+        //原有方法
+        Method originalMethod = class_getInstanceMethod(class, originalSelector);
+        //替换原有方法的新方法
+        Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
+        //先尝试給源SEL添加IMP，这里是为了避免源SEL没有实现IMP的情况（这种情况是原方法继承自父类，而本身并没有实现）
+        BOOL didAddMethod = class_addMethod(class,originalSelector,
+                                            method_getImplementation(swizzledMethod),
+                                            method_getTypeEncoding(swizzledMethod));
+        if (didAddMethod) {//添加成功：说明源SEL没有实现IMP，将源SEL的IMP替换到交换SEL的IMP
+            class_replaceMethod(class,swizzledSelector,
+                                method_getImplementation(originalMethod),
+                                method_getTypeEncoding(originalMethod));
+        } else {//添加失败：说明源SEL已经有IMP，直接将两个SEL的IMP交换即可
+            method_exchangeImplementations(originalMethod, swizzledMethod);
+        }
+    });
+}
+*/
 
 + (instancetype)create
 {
@@ -29,12 +56,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    
-    Class cls = [Person class];
-    
-    void *obj = &cls;
-    
-    [(__bridge id)obj print];
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
 }
 
 
@@ -50,7 +76,7 @@
     {
         return;
     }
-    
+
     [self searchKey:key];
 }
 
